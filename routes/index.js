6b88cfbd4b560cc61173d4d80ed4a01c7d72c859
BowmanFlooring <dakotabbowman@outlook.com>
2021-08-05 09:01:39 +0000

    Initial Commit: initializing the "'We' Over 'Us'" npm package

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.render('form', { title: 'Registration Form' });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  res.render('form', { title: 'Registration form' });
+});
+
+module.exports = router;
