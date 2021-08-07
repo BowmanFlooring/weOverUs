2c183eff26d2cb3e4629e046e200f258cdeb3e39
BowmanFlooring <dakotabbowman@outlook.com>
2021-08-07 15:17:09 +0000

    custom boilerplate designing is complete.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,59 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const { check, validationResult } = require('express-validator');
+
+const path = require('path');
+const auth = require('http-auth');
+
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
+
 
 const router = express.Router();
+const Registration = mongoose.model('Registration');
 
 router.get('/', (req, res) => {
   res.render('form', { title: 'Registration Form' });
 });
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  res.render('form', { title: 'Registration form' });
-});
+router.get('/registrations', basic.check((req, res) => {
+  Registration.find()
+    .then((registrations) => {
+      res.render('index', { title: 'Listing registrations', registrations });
+    })
+    .catch(() => { res.send('Sorry! Something went wrong.'); });
+}));
+
+router.post('/',
+  [
+    check('name')
+      .isLength({ min: 1 })
+      .withMessage('Please enter a name'),
+    check('email')
+      .isLength({ min: 1 })
+      .withMessage('Please enter an email'),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      const registration = new Registration(req.body);
+      registration.save()
+        .then(() => { res.send('Thank you for your registration!'); })
+        .catch((err) => {
+          console.log(err);
+          res.send('Sorry! Something went wrong.');
+        });
+    } else {
+      res.render('form', {
+        title: 'Registration form',
+        errors: errors.array(),
+        data: req.body,
+      });
+    }
+  }
+);
 
 module.exports = router;
