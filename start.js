ec66918dbb4884061045138e4fd372810b7b03a1
BowmanFlooring <dakotabbowman@outlook.com>
2021-08-08 00:15:25 +0000

    edit: took .env off of git's ignore list for local cloning purposes

--- start.js
+++ start.js
@@ -18,5 +18,5 @@ require('./models/Registration');
 const app = require('./app');
 
 const server = app.listen(3000, () => {
-  console.log('Express is running on port ${server.address().port}');
+  console.log('Express is running on port ${ server.address().port }');
 });
