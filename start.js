87f64f081bd4485c1b1845af1e34b854487609ff
BowmanFlooring <dakotabbowman@outlook.com>
2021-08-14 02:53:27 +0000

    various settings/config changes

--- start.js
+++ start.js
@@ -21,5 +21,5 @@ require('./models/Registration');
 const app = require('./app');
 
 const server = app.listen(3000, () => {
-  console.log('Express is running on port ${ server.address().port }');
+  console.log('Express is running on port {server.address().port}');
 });
