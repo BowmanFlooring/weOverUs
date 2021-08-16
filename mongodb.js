f863410f706f8b8301495108d0fd253bb7b40d2f
BowmanFlooring <dakotabbowman@outlook.com>
2021-08-16 03:06:34 +0000

    ops: updated settings.json, mongodb.js, Registration.js, .gitignore, and package(-lock).json

--- mongodb.js
+++ mongodb.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
 const DATABASE = 'mongodb+srv://dakota:bowmanfc@milkywaygalaxycluster.bkje3.mongodb.net/theSolarSystem?retryWrites=true&w=majority';
-=======
-const DATABASE = 'mongodb+srv://bfc_owner:bowmanfc@milkywaygalaxycluster.bkje3.mongodb.net/Andromeda?retryWrites=true&w=majority';
->>>>>>> da516fbce1d4554920267bf495ed4aedc6afecda
 
-module.exports = DATABASE;
+export default DATABASE;
